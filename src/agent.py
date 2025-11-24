d06af2a74b63b7daa1564b89190631854e7240b8
Marcelo Cardoso <mr.erykcardoso@gmail.com>
2025-11-24 01:03:07 -0300

    feat: implementação do menu de interação com o usuário (main.py) e do agente baseado em objetivo (agente.py); OBS: Aguardo de correções futuras com base no desenvolvimento da função de A*;

--- src/agent.py
+++ src/agent.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import algorithms_search as a
+from graphModel import Graph
 
 
 class Agent:
@@ -16,22 +17,32 @@ class Agent:
     def plan(self):
         """Função para o planejamento de tragetória para ação"""
         try:
-            self.path = a.aStar(self.graph, self.actual, self.goal); #função separada de A* usando networkx 
+            #self.path = a.aStar(self.graph, self.actual, self.goal); #função separada de A* usando networkx
+            # 1. Converter para networkx
+            G = self.graph.to_networkx()
+
+            # 2. Criar heurística adaptada
+            heuristic = lambda u, v: Graph.heuristic(
+                self.graph.positions[u], 
+                self.graph.positions[v]
+            )
+
+            # 3. Executar A*
+            self.path = nx.astar_path(G, self.actual, self.goal, heuristic=heuristic, weight='weight')
             print(f"Plano traçado: {self.path}");
         except Exception as e:
             print(f"Nenhum caminho encontrado. Erro: {e}");
             
     def act(self):
         """Execução do plano"""
-        print(f"\n\nCaminho planejado: {self.path};");
+        if not self.path:
+            print("\n\nImpossível prosseguir: Nenhum plano foi traçado ainda!");
+            return;
         
+        print(f"\n\nCaminho planejado: {self.path};");        
         while(self.actual != self.goal):
             
-            if not self.path:
-                print("\n\nImpossível prosseguir: Nenhum plano foi traçado ainda!");
-                return;
-            
-            elif self.path and self.actual != self.goal:
+            if self.path and self.actual != self.goal:
                 self.path.pop(0); 
                 if self.path:
                     self.actual = self.path[0];
